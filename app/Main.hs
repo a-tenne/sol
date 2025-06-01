996deda9ccbb0faa0bb150652659994ae6bfdc0e
a-tenne <atenne@web.de>
2025-06-01 12:38:08 +0200

    gitignore and file read

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,27 @@
 module Main where
 
+import System.Environment
+import Text.Parsec (digit, skipMany)
+import Text.Parsec.Char (space)
+import Text.Parsec.Combinator (many1)
+import Text.Parsec.String (Parser)
+
+num :: Parser Integer
+num = do
+  n <- many1 digit
+  return (read n)
+
+whitespace :: Parser ()
+whitespace = skipMany space
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  argv <- getArgs
+  let inFile = last argv
+  if null inFile
+    then
+      error "Input file required!"
+    else do
+      fileHandle <- readFile inFile
+      putStr fileHandle
+      return ()
